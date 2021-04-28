3129a4c3d50919e5f03bf638c2077079a33e7588
Lea <Lea@DESKTOP-LFHUJIE>
2021-04-28 17:50:11 -0300

    comi1

--- src/ar/edu/unq/po2/punto/Punto.java
+++ src/ar/edu/unq/po2/punto/Punto.java
@@ -6,19 +6,13 @@ public class Punto {
 	private float y;
 	public Punto(float x, float y) {
 		super();
-<<<<<<< Upstream, based on branch 'main' of https://github.com/LeandroOtel/unqui-po2-otel.git
 		this.setXY(x, y);
 	}
 	public void setXY(float x,float y) {
 		this.setX(x);
 		this.setY(y);
 	}
-	
-=======
-		this.x = x;
-		this.y = y;
-	}
->>>>>>> 3f78c26 Versión inicial
+
 	public float getX() {
 		return x;
 	}
