a0d7b6ace95c44b703379c269604297555bdbf29
Lea <Lea@DESKTOP-LFHUJIE>
2021-04-29 15:40:31 -0300

    ejercicio5 tp3

---
+++ src/ar/edu/unq/po2/tp3/ejercicio5/JerarquiaDePaquetes.java
@@ -0,0 +1,16 @@
+package ar.edu.unq.po2.tp3.ejercicio5;
+
+public class JerarquiaDePaquetes {
+	/*Cree una clase que se encuentre en el paquete model, otra clase que se encuentre en el paquete model.gui y
+	otra que se encuentre en el paquete model.stack . Compílelas utilizando la forma que crea conveniente.
+	
+	1. ¿Cómo están organizadas en el sistema de archivos?
+	
+	Una carpeta raíz llamada model, con sus 2 ramas model.stack y model.gui
+	
+	2. ¿Encuentra alguna relación entre el nombre del paquete y la ubicación de los archivos fuentes de las
+	clases (.java) y los archivos compilados (.class)?
+	
+	Las clases.class se encuentran en el paquete .bin y en el .src se encuentran los archivos.java
+	*/
+}
