5de734c13ac32caf224977a59f6d9a4f577f38bc
Lea <Lea@DESKTOP-LFHUJIE>
2021-05-03 18:53:02 -0300

    cuadrado

---
+++ src/ar/edu/unq/po2/tp3/ejercicio7/Cuadrado.java
@@ -0,0 +1,12 @@
+package ar.edu.unq.po2.tp3.ejercicio7;
+
+import ar.edu.unq.po2.tp3.ejercicio6.Point;
+
+public class Cuadrado extends Rectangulo {
+	
+	
+	
+	public Cuadrado(Point esqInferiorIzquierda, Integer lado) {
+		super(esqInferiorIzquierda, lado, lado);
+	}
+}
--- test/ar/edu/unq/po2/tp3/ejercicio7/RectanguloTestCase.java
+++ test/ar/edu/unq/po2/tp3/ejercicio7/RectanguloTestCase.java
@@ -2,6 +2,7 @@ package ar.edu.unq.po2.tp3.ejercicio7;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,7 @@ class RectanguloTestCase {
 	
 	private Rectangulo rectangulo;
 	private Point punto11;
+	private Cuadrado cuadrado;
 	//private Point punto14;
 	//private Point punto41;
 	//private Point punto44;
@@ -26,6 +28,7 @@ class RectanguloTestCase {
 		//this.punto41 = new Point(4,1);
 		//this.punto44 = new Point(4,4);
 		this.rectangulo=new Rectangulo(punto11, 2, 3);
+		this.cuadrado= new Cuadrado(punto11, 3);
 		
 		
 	}
@@ -60,7 +63,7 @@ class RectanguloTestCase {
 	}
 	
 	@Test
-	void testArea() {
+	void testAreaRectangulo() {
 		assertEquals(this.rectangulo.area(), 6);
 	}
 	
@@ -68,7 +71,7 @@ class RectanguloTestCase {
 	
 	
 	@Test
-	void testPerimetro() {
+	void testPerimetroRectangulo() {
 		assertEquals(this.rectangulo.perimetro(), 10);
 	}
 	
@@ -78,5 +81,16 @@ class RectanguloTestCase {
 	@Test
 	void tipoDeRectangulo() {
 		assertEquals(this.rectangulo.tipo(), "horizontal");
+	}
+	
+	@Test
+	void testAreaCuadrado() {
+		assertEquals(this.cuadrado.area(), 9);
+	}
+	
+	
+	@Test
+	void testPerimetroCuadrado() {
+		assertEquals(this.cuadrado.perimetro(), 12);
 	}
 }
