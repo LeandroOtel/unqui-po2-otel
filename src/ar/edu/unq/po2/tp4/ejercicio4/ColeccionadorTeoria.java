09bfd60a479b88034628710665be022855f5b287
Lea <Lea@DESKTOP-LFHUJIE>
2021-05-18 14:01:07 -0300

    ej4 tp4 teoria

---
+++ src/ar/edu/unq/po2/tp4/ejercicio4/ColeccionadorTeoria.java
@@ -0,0 +1,28 @@
+package ar.edu.unq.po2.tp4.ejercicio4;
+
+public class ColeccionadorTeoria {
+	/*
+	1)GetFirstFrom devolverá el primer elemento de la colección. Esto dependerá 
+	si la colección que elijamos mantiene o no un orden. Por ejemplo el caso de 
+	los Bags y multisets no tienen ese orden. En cambio, las listas son 
+	colecciones ordenadas (ej. ArrayList).
+
+
+
+	2)AddLast funcionará con las listas, ya que al agregar un elemento va 
+	directamente al final.
+
+
+
+	3)GetSubCollection también con las listas, devuelve una parte de la colección
+	 desde el índice marcado hasta índice2.
+
+
+
+	4)El containsElement funcionará en cualquier coleccion que elijamos, 
+	devuelve si la coleccion corredspondiente contiene dicho elemento indicado.
+
+
+
+	5)Es posible en las colecciones tanto usar Clases como Interfaces.*/
+}
