4a0521eb05c9febacbdab8ed309b04eb9cc2605a
Lea <Lea@DESKTOP-LFHUJIE>
2021-05-26 19:45:39 -0300

    teoria tp8 ej1

---
+++ src/ar/edu/unq/po2/tp8/ej1/Teoria.java
@@ -0,0 +1,31 @@
+package ar.edu.unq.po2.tp8.ej1;
+
+public class Teoria {
+ /*
+  * 1.En la intención del patrón se indica que el cliente debe 
+  * poder tratar de manera uniforme tanto a objetos individuales 
+  * como compuestos. ¿ Cómo se logra esto con el patrón Composite?
+  * 
+  * Tanto el objeto individual como el compuesto implementan la misma
+  * interface, por lo tanto pueden recibir los mismos mensajes.
+  * De esta manera, el cliente se abstrae de
+  * que estructura esta manejando.
+  * 
+  * 2. En qué casos aplicaría el patrón ? ? 
+  * 
+  * Se utiliza cuando se quieren representar objetos simples
+  * o complejos. Cuando el cliente quiere ignorar la diferencia entre
+  * estos mismos elementos simples y compuestos. Simplifica el codigo
+  * del cliente.
+  * Ej archivos y carpetas(contenedoras de otras carpetas o archivos).
+  * 
+  * 
+  * 3.Dentro de las consecuencias, se indica que los clientes deben 
+  * mantenerse simples y no deben ser conscientes si están 
+  * interactucando con un objeto hoja o una composición. Que elementos 
+  * de la programación orientada a objetos hacen posible esto? 
+  * 
+  * La interface, o en algunos casos la clase abstracta. Permite al 
+  * cliente poder abstraerse del objeto con el cual esta trabajando.
+  * */
+}
