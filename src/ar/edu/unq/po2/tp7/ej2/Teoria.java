25afb94d78ea1d28ad434eba469a29694df4bdab
Lea <Lea@DESKTOP-LFHUJIE>
2021-06-04 18:53:39 -0300

    teoria

---
+++ src/ar/edu/unq/po2/tp7/ej2/Teoria.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.po2.tp7.ej2;
+
+public class Teoria {
+	/*
+	 * 1. ¿Que impacto genera en su diseño el hecho de compartir instancias de parcelas entre composiciones?
+	 *  Es decir, que la misma instancia se encuentre en más de una composición.
+	 */
+}
