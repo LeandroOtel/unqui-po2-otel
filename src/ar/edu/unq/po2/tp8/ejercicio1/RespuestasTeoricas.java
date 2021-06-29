3ced54127d10a9c2926302e67dc21e9bed0f60d9
Lea <Lea@DESKTOP-LFHUJIE>
2021-06-29 16:18:33 -0300

    respuestas teoricas

---
+++ src/ar/edu/unq/po2/tp8/ejercicio1/RespuestasTeoricas.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.po2.tp8.ejercicio1;
+
+public class RespuestasTeoricas {
+	/*
+	 * 1. donde se define el cuerpo del algoritmo?
+
+		En la clase abstracta
+
+		2.se puede redefinir el esqueleto?
+
+		No, tienen alcance "final" porque las subclases dependen del mismo. 
+		Queres hacer una plantilla que las subclases respeten.
+
+		3.que es lo que se puede redefinir?
+		
+		Los métodos de las subclases, los pasos que varían sin cambiar la estructura del 
+		algoritmo.
+		
+		4.que es un hook method?
+		
+		métodos de los cuales uno se engancha para poder agregar comportamiento.
+		Por ejemplo en la clase abstracta ponemos un método vacío, que no hace nada y 
+		cuando alguna subclase lo implemente ahí lo redefinimos.
+		
+	 */
+}
