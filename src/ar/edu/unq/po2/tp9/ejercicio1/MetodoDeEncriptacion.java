5a8ad7e59f627f01008b81debb55c7b306fa2ea7
Lea <Lea@DESKTOP-LFHUJIE>
2021-07-02 20:07:13 -0300

    metodo

---
+++ src/ar/edu/unq/po2/tp9/ejercicio1/MetodoDeEncriptacion.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.po2.tp9.ejercicio1;
+
+public abstract class MetodoDeEncriptacion {
+	
+	public abstract String encriptar(String texto);
+
+	public abstract String desencriptar(String texto);
+}
