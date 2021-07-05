4ad605d3e85848fd5da2e524aeeea7610b45b152
Lea <Lea@DESKTOP-LFHUJIE>
2021-07-05 17:47:48 -0300

    lista

---
+++ test/ar/edu/unq/po2/tp8/ejercicio7/ListaDePalabrasOrdenadasAdapter.java
@@ -0,0 +1,18 @@
+package ar.edu.unq.po2.tp8.ejercicio7;
+
+import javax.swing.DefaultListModel;
+
+public class ListaDePalabrasOrdenadasAdapter extends DefaultListModel<String>{
+	private ListaDePalabrasOrdenadas lista;
+	
+	public ListaDePalabrasOrdenadasAdapter(ListaDePalabrasOrdenadas listaO) {
+		this.lista = listaO;
+	}
+	
+	@Override
+	public void addElement(String element) {
+		lista.agregarPalabra(element);
+	}
+
+	
+}
